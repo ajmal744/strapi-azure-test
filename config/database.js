6866fd1c4b06fafb8494271479b92578c7dc7b94
Muhammad Khan <muhammadkhan@corlife.com>
2022-03-09 18:43:29 +0000

    added mysql

--- config/database.js
+++ config/database.js
@@ -1,11 +1,19 @@
 const path = require('path');
 
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'mysql',
+        host: env('DATABASE_HOST', 'my-strapi-db-mo.mysql.database.azure.com'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'makhan'),
+        password: env('DATABASE_PASSWORD', 'March2022'),
+      },
+      options: {},
     },
-    useNullAsDefault: true,
   },
-});
+})
