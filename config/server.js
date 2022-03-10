8ab76a25a20189fd8f4b185440b38ee52c76953e
Muhammad Khan <muhammadkhan@corlife.com>
2022-03-10 12:53:12 +0000

    added keys

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: ['+YgQTjuA/6ueujH0RUM0Ow==','Z73//cYh1dD6d8qkSFxqxQ==','kAFt4KHUiOoreHWf6aixiA==','T/+LAiQUnWz2Z2GwlFhlPg=='],
   },
 });
